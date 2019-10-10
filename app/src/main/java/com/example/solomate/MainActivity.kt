f1b4e7826998a6947e8443a8c689de1c77617f95
Preetish <niketpreetish@gmail.com>
2019-10-10 23:13:32 +0530

    Register here Intent

--- app/src/main/java/com/example/solomate/MainActivity.kt
+++ app/src/main/java/com/example/solomate/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.example.solomate
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    override fun onClick(v: View?) {
+        val i =Intent(this, Registerhere::class.java)
+        startActivity(i)
+        val a=Intent(this,Login::class.java)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        textView2.setOnClickListener(this)
+        li.setOnClickListener(this)
     }
 }
---
+++ app/src/main/java/com/example/solomate/Registerhere.kt
@@ -0,0 +1,12 @@
+package com.example.solomate
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Registerhere : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registerhere)
+    }
+}
